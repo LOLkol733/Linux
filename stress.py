858c8d4805433395169fb7ab4ca06de7a38570c9
LOLkol733 <149338633+LOLkol733@users.noreply.github.com>
2024-07-30 22:28:02 +1000

    Create stress.py

---
+++ stress.py
@@ -0,0 +1,15 @@
+from hashlib import Lolkol733
+
+
+def hard():
+    hash384 = sha384('password'.encode())
+    c=0
+    while c < 50000:
+        hash384.update(hash384.digest())
+        c = c+1 
+    return { "status": 1, "message": hash384.hexdigest() }  
+
+
+if __name__ == "__main__":
+    print(hard())
+ 
